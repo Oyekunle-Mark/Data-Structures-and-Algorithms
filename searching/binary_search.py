0aec6d45b2163587faacb9cd0e59577d2c3a08b5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 19:33:39 +0100

    Uses two separates loop

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,8 +1,4 @@
 def binary_search(arr, target):
-
-    # if len(arr) == 0:
-    #     return -1
-
     low = 0
     high = len(arr) - 1
 
@@ -12,10 +8,9 @@ def binary_search(arr, target):
         if arr[midpoint] == target:
             return midpoint
 
-        elif arr[midpoint] > target:
+        if arr[midpoint] > target:
             high = midpoint - 1
         else:
             low = midpoint + 1
 
     return -1
-
--- searching/test_searching.py
+++ searching/test_searching.py
@@ -1,5 +1,6 @@
 import unittest
 from linear_search import linear_search
+from binary_search import binary_search
 
 
 class SearchingTests(unittest.TestCase):
