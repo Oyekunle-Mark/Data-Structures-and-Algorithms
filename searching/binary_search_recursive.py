07a01444f5bea5bf499de44edddbfd86ae04fdb1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 19:38:51 +0100

    binary search recursive first pass

--- searching/binary_search_recursive.py
+++ searching/binary_search_recursive.py
@@ -1,5 +1,13 @@
 def binary_search_recursive(arr, target, low, high):
-  middle = (low+high)//2
+    if len(arr) == 0:
+        return -1
 
-  if len(arr) == 0:
-    return -1
+    if arr[low] == target:
+        return low
+
+    midpoint = (low + high) // 2
+
+    if arr[midpoint] > target:
+        return binary_search_recursive(arr, target, low, midpoint - 1)
+    else:
+        return binary_search_recursive(arr, target, midpoint + 1, high)
