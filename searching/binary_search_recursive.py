e699e188598738ce33a031a98632bc19ae346182
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 19:54:24 +0100

    Used list slicing instaed

--- searching/binary_search_recursive.py
+++ searching/binary_search_recursive.py
@@ -1,5 +1,5 @@
 def binary_search_recursive(arr, target, low, high):
-    if len(arr) <= 0:
+    if len(arr) == 0:
         return -1
 
     midpoint = (low + high) // 2
@@ -8,6 +8,6 @@ def binary_search_recursive(arr, target, low, high):
         return midpoint
 
     if arr[midpoint] > target:
-        return binary_search_recursive(arr, target, low, midpoint - 1)
+        return binary_search_recursive(arr[:midpoint], target, low, midpoint - 1)
     else:
-        return binary_search_recursive(arr, target, midpoint + 1, high)
+        return binary_search_recursive(arr[midpoint:], target, midpoint + 1, high)
