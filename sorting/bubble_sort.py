201ac916b1b8e44c1b9a9ce488870d6de3e27eb1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-19 20:03:54 +0100

    The first pass solution

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,3 +1,7 @@
 def bubble_sort(arr):
-
+    for i in range(len(arr)):
+        for j in range(i + 1):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    
     return arr
