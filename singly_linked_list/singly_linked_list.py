e3dd714be2b95806992bd2b03503305e46c967d8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-25 09:11:11 +0100

    Boiler plate for the linked list class and methods

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,6 +1,7 @@
 class ListNode:
     """A single linked list node
     """
+
     def __init__(self, value, next=None):
         pass
 
@@ -11,4 +12,20 @@ class ListNode:
         pass
 
     def set_next(self):
+        pass
+
+
+class LinkedList:
+    """The linked list class
+    """
+    def __init__(self):
+        pass
+
+    def add_to_tail(self, value):
+        pass
+
+    def remove_head(self):
+        pass
+
+    def contains(self, value):
         pass
