daa35167d3dca15edf6d9f841b8a9338b3192045
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-27 16:23:32 +0100

    Does dft print on the nodes

--- binary_search_tree/bst.py
+++ binary_search_tree/bst.py
@@ -85,7 +85,18 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+
+        while stack.len():
+            current_node = stack.pop()
+            print(current_node.value)
+
+            if current_node.left is not None:
+                stack.push(current_node.left)
+
+            if current_node.right is not None:
+                stack.push(current_node.right)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
