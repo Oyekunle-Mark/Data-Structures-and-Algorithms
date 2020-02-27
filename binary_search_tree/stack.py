3f01a932b3c883242343b0552d83910d8962bf41
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-27 13:12:45 +0100

    Th e stack class

---
+++ binary_search_tree/stack.py
@@ -0,0 +1,19 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = DoublyLinkedList()
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size:
+            self.size -= 1
+            return self.storage.remove_from_head()
+
+    def len(self):
+        return self.size
