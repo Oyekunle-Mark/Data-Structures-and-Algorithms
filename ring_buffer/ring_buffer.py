0653196a46a7dcde10186f54bb304e23a68597d8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-28 11:37:49 +0100

    Pseudcode for append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,7 +8,16 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if the length of the DoublyLinkedList/dll is less than the capacity
+            # add the item to the tail
+            # set the current to the tail of the dll
+        # if the length of the dll is equivalent to the capacity of the buffer
+            # if the current node is the tail of the dll
+                # set the current to the head of the dll
+            # otherwise,
+                # set the current to the next node after it
+
+            # set the value of current to the item
 
     def get(self):
         # Note:  This is the only [] allowed
