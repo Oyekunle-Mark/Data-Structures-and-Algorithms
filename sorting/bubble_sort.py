7ce134165ca511d47017422275bfe5ff4a38cbc0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-07 11:12:37 +0100

    Pretty formatted the code

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -3,10 +3,13 @@ from typing import List
 
 def bubble_sort(arr: List[int]) -> List[int]:
     swapped = True
+
     while swapped:
         swapped = False
+
         for i in range(len(arr) - 1):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 swapped = True
+
     return arr
