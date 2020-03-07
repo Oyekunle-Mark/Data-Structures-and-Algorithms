7dbdcf8d888801d568b1f2560cb98348fc4aec2d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-07 12:15:25 +0100

    Added type annotations to the english ruler algorithm

--- recursion/english_ruler.py
+++ recursion/english_ruler.py
@@ -3,7 +3,10 @@ The classic English Ruler problem. The function draw_ruler takes the
 numbers of inches to be drawn and the tick_length of the ruler
 """
 
-def draw_line(tick_length, label=''):
+from typing import Optional
+
+
+def draw_line(tick_length: int, label: Optional[str] = '') -> None:
     line = '-' * tick_length
 
     if label:
@@ -12,14 +15,14 @@ def draw_line(tick_length, label=''):
     print(line)
 
 
-def draw_interval(tick_length):
+def draw_interval(tick_length: int) -> None:
     if tick_length > 0:
         draw_interval(tick_length - 1)
         draw_line(tick_length)
         draw_interval(tick_length - 1)
 
 
-def draw_ruler(inches, tick_length):
+def draw_ruler(inches: int, tick_length: int) -> None:
     draw_line(tick_length, "0")
 
     for num in range(1, inches + 1):
