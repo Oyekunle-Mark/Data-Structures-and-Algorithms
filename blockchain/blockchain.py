9a1a6487bccfc0244371716ea5970fd8ac0120f6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-11 22:37:19 +0100

    Proof of work with six leading zeros

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -116,7 +116,7 @@ class Blockchain(object):
         # create a guess hash and hexdigest it
         guess_hash = hashlib.sha256(guess).hexdigest()
         # then return True if the guess hash has the valid number of leading zeros otherwise return False
-        return guess_hash[:3] == "000"
+        return guess_hash[:6] == "000000"
 
 
 # Instantiate our Node
