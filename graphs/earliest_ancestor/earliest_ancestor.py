e3bfe9565f68437b3a63ffddb42e22e96ec663e1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 11:41:39 +0100

    Added test cases to the main file

--- graphs/earliest_ancestor/earliest_ancestor.py
+++ graphs/earliest_ancestor/earliest_ancestor.py
@@ -12,3 +12,18 @@ def earliest_ancestor(ancestors, starting_node):
         farthest_ancestor = find_ancestor(ancestors, farthest_ancestor)
 
     return farthest_ancestor
+
+if __name__ == '__main__':
+    ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+    print(earliest_ancestor(ancestors, 1))
+    print(earliest_ancestor(ancestors, 2))
+    print(earliest_ancestor(ancestors, 3))
+    print(earliest_ancestor(ancestors, 4))
+    print(earliest_ancestor(ancestors, 5))
+    print(earliest_ancestor(ancestors, 6))
+    print(earliest_ancestor(ancestors, 7))
+    print(earliest_ancestor(ancestors, 8))
+    print(earliest_ancestor(ancestors, 9))
+    print(earliest_ancestor(ancestors, 10))
+    print(earliest_ancestor(ancestors, 11))
