b2484864ba64dbc0a0bf66c7c715073a4b3facbf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 12:02:08 +0100

    First pass solution

--- graphs/earliest_ancestor/earliest_ancestor.py
+++ graphs/earliest_ancestor/earliest_ancestor.py
@@ -3,15 +3,23 @@ def find_ancestor(ancestors, num):
         if child == num:
             return parent
 
-    return -1
+    return None
 
 def earliest_ancestor(ancestors, starting_node):
     farthest_ancestor = find_ancestor(ancestors, starting_node)
 
-    while farthest_ancestor:
+    if farthest_ancestor is None:
+        return -1
+
+    while True:
+        if find_ancestor(ancestors, farthest_ancestor) is None:
+            return farthest_ancestor
         farthest_ancestor = find_ancestor(ancestors, farthest_ancestor)
 
+        
+
     return farthest_ancestor
+
 
 if __name__ == '__main__':
     ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
