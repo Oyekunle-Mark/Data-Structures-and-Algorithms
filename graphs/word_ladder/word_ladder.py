d4522101b68a6bfeb9fe800457734f7d5c3ec9ed
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 09:08:25 +0100

    The bare bones structure

---
+++ graphs/word_ladder/word_ladder.py
@@ -0,0 +1,9 @@
+from util import Queue
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def find_neighbors(word):
+    pass
+
+def word_ladder(beginWord, endWord):
+    pass
