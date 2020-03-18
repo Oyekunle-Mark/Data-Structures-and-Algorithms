9bf80e0efb4de25903531437a8c82abde801d697
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 09:17:33 +0100

    Added docstring to the functions

--- graphs/word_ladder/word_ladder.py
+++ graphs/word_ladder/word_ladder.py
@@ -20,7 +20,17 @@ LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 
 
 def find_neighbors(word):
+    """
+    Find all the possible neighbors of a word by replacing each letters 
+    in the word with every letter in the English alphabet and confirming if
+    the generated word can be found in word_set. It returns all the possible neighbors
+    as a list of strings
+    """
     pass
 
 def word_ladder(beginWord, endWord):
+    """
+    Uses Breadth-First Search(BFS) to find the transformation sequence of a word
+    from beginWord to endWord.
+    """
     pass
