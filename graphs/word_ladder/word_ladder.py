097564a5a60c1d49bf9c5fb3cd2055b205697112
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 09:13:11 +0100

    Imports the words and adds them to the word_set set

--- graphs/word_ladder/word_ladder.py
+++ graphs/word_ladder/word_ladder.py
@@ -1,6 +1,16 @@
 from util import Queue
 
+word_set = set()
+words = []
+
+with open('words.txt', 'r') as f:
+    words = f.read().split('\n')
+
+for word in words:
+    word_set.add(word)
+
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 def find_neighbors(word):
     pass
