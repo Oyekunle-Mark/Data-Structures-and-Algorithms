72f6e99f595a626b0c8693e129aeab28e7ae983e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 09:14:44 +0100

    Commented the codes

--- graphs/word_ladder/word_ladder.py
+++ graphs/word_ladder/word_ladder.py
@@ -1,14 +1,21 @@
 from util import Queue
 
+# would hold the words
 word_set = set()
+# global variable to add words from file to
 words = []
 
+# open and file
 with open('words.txt', 'r') as f:
+    # read all the words, splitting on new lines
     words = f.read().split('\n')
 
+# for every word words
 for word in words:
+    # add to word_set
     word_set.add(word)
 
+# letters in the English alphabet
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
