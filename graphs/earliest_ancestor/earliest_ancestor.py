49e16c4398d42b421d3a5c1d15149730a12c995c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 11:31:04 +0100

    Frist pass for find_ancestor

--- graphs/earliest_ancestor/earliest_ancestor.py
+++ graphs/earliest_ancestor/earliest_ancestor.py
@@ -1,5 +1,9 @@
-def find_ancestor(num):
-    pass
+def find_ancestor(ancestors, num):
+    for parent, child in ancestors:
+        if child == num:
+            return parent
+
+    return -1
 
 def earliest_ancestor(ancestors, starting_node):
     pass
