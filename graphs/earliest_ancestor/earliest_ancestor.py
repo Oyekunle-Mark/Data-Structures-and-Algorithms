2cb770d41c7760bd6c1067d4ffe1f59846e1ca18
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-18 16:04:31 +0100

    Autoformtted the ancestor

--- graphs/earliest_ancestor/earliest_ancestor.py
+++ graphs/earliest_ancestor/earliest_ancestor.py
@@ -44,9 +44,9 @@ def earliest_ancestor(ancestors: AncestorsType, starting_node: int) -> int:
         farthest_ancestor = find_ancestor(ancestors, farthest_ancestor)
 
 
-
 if __name__ == '__main__':
-    ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                 (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
     print(earliest_ancestor(ancestors, 1))
     print(earliest_ancestor(ancestors, 2))
